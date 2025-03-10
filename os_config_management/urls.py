c9414938e872140c7f8013d761dd9fd9a1453d83
Eric Hester <eric.hester@umbrella.tech>
2025-03-10 12:03:46 -0400

    .

--- os_config_management/urls.py
+++ os_config_management/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('config-items/<int:pk>/delete/', views.ConfigItemDeleteView.as_view(), name='configitem_delete'),
     path('config-items/bulk-edit/', views.ConfigItemBulkEditView.as_view(), name='configitem_bulk_edit'),
     path('config-items/bulk-delete/', views.ConfigItemBulkDeleteView.as_view(), name='configitem_bulk_delete'),
-    path('config-items/changelog/', views.ConfigItemChangeLogView.as_view(), name='configitem_changelog', kwargs={'model': models.ConfigItem}),
+    path('config-items/<int:pk>/changelog/', views.ConfigItemChangeLogView.as_view(), name='configitem_changelog', kwargs={'model': models.ConfigItem}),
 
     # ConfigSet URLs
     path('config-sets/', views.ConfigSetListView.as_view(), name='configset_list'),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('config-sets/<int:pk>/delete/', views.ConfigSetDeleteView.as_view(), name='configset_delete'),
     path('config-sets/bulk-edit/', views.ConfigSetBulkEditView.as_view(), name='configset_bulk_edit'),
     path('config-sets/bulk-delete/', views.ConfigSetBulkDeleteView.as_view(), name='configset_bulk_delete'),
-    path('config-sets/changelog/', views.ConfigSetChangeLogView.as_view(), name='configset_changelog', kwargs={'model': models.ConfigSet}),
+    path('config-sets/<int:pk>/changelog/', views.ConfigSetChangeLogView.as_view(), name='configset_changelog', kwargs={'model': models.ConfigSet}),
     
     # OSConfig URLs
     path('os-configs/', views.OSConfigListView.as_view(), name='osconfig_list'),
@@ -34,5 +34,5 @@ urlpatterns = [
     path('os-configs/<int:pk>/delete/', views.OSConfigDeleteView.as_view(), name='osconfig_delete'),
     path('os-configs/bulk-edit/', views.OSConfigBulkEditView.as_view(), name='osconfig_bulk_edit'),
     path('os-configs/bulk-delete/', views.OSConfigBulkDeleteView.as_view(), name='osconfig_bulk_delete'),
-    path('os-configs/changelog/', views.OSConfigChangeLogView.as_view(), name='osconfig_changelog', kwargs={'model': models.OSConfig}),    
+    path('os-configs/<int:pk>/changelog/', views.OSConfigChangeLogView.as_view(), name='osconfig_changelog', kwargs={'model': models.OSConfig}),    
 ]
