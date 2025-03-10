0f411473de63077bb69aedaf9bc7c7ce0ef4a05c
Eric Hester <eric.hester@umbrella.tech>
2025-03-10 10:50:49 -0400

    .

--- os_config_management/navigation.py
+++ os_config_management/navigation.py
@@ -2,51 +2,51 @@ from netbox.plugins import PluginMenu, PluginMenuItem, PluginMenuButton
 
 items = (
     PluginMenuItem(
-        link='plugins:netbox_os_config:configitem_list',
+        link='plugins:os_config_management:configitem_list',
         link_text='Config Items',
         permissions=['netbox_os_config.view_configitem'],
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_os_config:configitem_add',
+                link='plugins:os_config_management:configitem_add',
                 title='Add',
                 icon_class='mdi mdi-plus',
             ),
             PluginMenuButton(
-                link='plugins:netbox_os_config:configitem_import',
+                link='plugins:os_config_management:configitem_import',
                 title='Import',
                 icon_class='mdi mdi-upload',
             ),
         )
     ),
     PluginMenuItem(
-        link='plugins:netbox_os_config:configset_list',
+        link='plugins:os_config_management:configset_list',
         link_text='Config Sets',
         permissions=['netbox_os_config.view_configset'],
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_os_config:configset_add',
+                link='plugins:os_config_management:configset_add',
                 title='Add',
                 icon_class='mdi mdi-plus',
             ),
             PluginMenuButton(
-                link='plugins:netbox_os_config:configset_import',
+                link='plugins:os_config_management:configset_import',
                 title='Import',
                 icon_class='mdi mdi-upload',
             ),
         )
     ),
     PluginMenuItem(
-        link='plugins:netbox_os_config:osconfig_list',
+        link='plugins:os_config_management:osconfig_list',
         link_text='OS Configurations',
         permissions=['netbox_os_config.view_osconfig'],
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_os_config:osconfig_add',
+                link='plugins:os_config_management:osconfig_add',
                 title='Add',
                 icon_class='mdi mdi-plus',
             ),
             PluginMenuButton(
-                link='plugins:netbox_os_config:osconfig_import',
+                link='plugins:os_config_management:osconfig_import',
                 title='Import',
                 icon_class='mdi mdi-upload',
             ),
